76be392c1586bd92386458cdc0781dc9abf11fe8
Ales <grm.ales@gmail.com>
2023-10-24 07:43:17 +0000

    Add git launch

---
+++ run_code-server.sh
@@ -0,0 +1,1 @@
+sudo docker run -d -it --name code-server -p 0.0.0.0:0080:8080   -v "$HOME/.ssh:/home/coder/.ssh"   -v "$HOME/.local:/home/coder/.local"   -v "$HOME/.config:/home/coder/.config"   -v "$PWD:/home/coder/project"   -u "$(id -u):$(id -g)"   -e "DOCKER_USER=$USER"   codercom/code-server:latest
