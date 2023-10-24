82c0a4b3b199c11f0f77c3f1f48d923022dc28a0
Ales <grm.ales@gmail.com>
2023-10-24 07:49:37 +0000

    Map git from local to container

--- run_code-server.sh
+++ run_code-server.sh
@@ -1,1 +1,2 @@
-sudo docker run -d -it --name code-server -p 0.0.0.0:0080:8080   -v "$HOME/.ssh:/home/coder/.ssh"   -v "$HOME/.local:/home/coder/.local"   -v "$HOME/.config:/home/coder/.config"   -v "$PWD:/home/coder/project"   -u "$(id -u):$(id -g)"   -e "DOCKER_USER=$USER"   codercom/code-server:latest
+mkdir "$HOME/git"
+sudo docker run -d -it --name code-server -p 0.0.0.0:0080:8080 -v "$HOME/git:/home/coder/git"  -v "$HOME/.ssh:/home/coder/.ssh"   -v "$HOME/.local:/home/coder/.local"   -v "$HOME/.config:/home/coder/.config"   -v "$PWD:/home/coder/project"   -u "$(id -u):$(id -g)"   -e "DOCKER_USER=$USER"   codercom/code-server:latest
